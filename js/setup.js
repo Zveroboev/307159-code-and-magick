e0a5ee24e9781d1b3f3bc6acb9e9241b71de0030
Sergey Zveroboev <szveroboev@gmail.com>
2017-08-16 21:55:45 +0300

    Вынес константы в начало кода

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var WIZARD_ATTRIBUTES = {
+  WIZARD_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  WIZARD_SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
+  COAT_COLORS: [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ]
+};
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
@@ -32,20 +46,6 @@ function renderWizard(wizard) {
 
   return wizardElement;
 }
-
-var WIZARD_ATTRIBUTES = {
-  WIZARD_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
-  WIZARD_SURNAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
-  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
-  COAT_COLORS: [
-    'rgb(101, 137, 164)',
-    'rgb(241, 43, 107)',
-    'rgb(146, 100, 161)',
-    'rgb(56, 159, 117)',
-    'rgb(215, 210, 55)',
-    'rgb(0, 0, 0)'
-  ]
-};
 
 var numberWizards = 4;
 var wizards = getArrayWizards(numberWizards);
