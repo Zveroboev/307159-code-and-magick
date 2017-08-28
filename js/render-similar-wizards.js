59c71ba5f0c8f81fc63196a74ad60aeeece2f759
Sergey Zveroboev <szveroboev@gmail.com>
2017-08-28 22:40:28 +0300

    Правка багов

--- js/render-similar-wizards.js
+++ js/render-similar-wizards.js
@@ -2,17 +2,19 @@
 
 (function () {
 
-  var wizards = getArrayWizards(window.WIZARD_ATTRIBUTES.WIZARDS_QUANTITY);
+  var WIZARDS = window.CONSTATNS.WIZARDS_ATTRIBUTES;
+
+  var similarWizards = getArrayWizards(WIZARDS.WIZARDS_QUANTITY);
 
   function getArrayWizards(numberWizards) {
     var wizardsArray = [];
 
     for (var i = 0; i < numberWizards; i++) {
       wizardsArray.push({
-        name: window.util.getRandomValue(window.WIZARD_ATTRIBUTES.WIZARD_NAMES) + ' ' +
-        window.util.getRandomValue(window.WIZARD_ATTRIBUTES.WIZARD_SURNAMES),
-        coatColor: window.util.getRandomValue(window.WIZARD_ATTRIBUTES.COAT_COLORS),
-        eyesColor: window.util.getRandomValue(window.WIZARD_ATTRIBUTES.EYES_COLORS)
+        name: window.util.getRandomValue(WIZARDS.WIZARD_NAMES) + ' ' +
+        window.util.getRandomValue(WIZARDS.WIZARD_SURNAMES),
+        coatColor: window.util.getRandomValue(WIZARDS.COAT_COLORS),
+        eyesColor: window.util.getRandomValue(WIZARDS.EYES_COLORS)
       });
     }
     return wizardsArray;
@@ -28,17 +30,17 @@
     return wizardElement;
   }
 
-  function renderSimilarWizards(similarWizards) {
+  function renderSimilarWizards(wizardsArray) {
     var userDialog = document.querySelector('.setup');
     var similarListElement = userDialog.querySelector('.setup-similar-list');
 
-    for (var i = 0; i < similarWizards.length; i++) {
-      similarListElement.appendChild(renderWizard(similarWizards[i]));
+    for (var i = 0; i < wizardsArray.length; i++) {
+      similarListElement.appendChild(renderWizard(wizardsArray[i]));
     }
 
     userDialog.querySelector('.setup-similar').classList.remove('hidden');
   }
 
-  renderSimilarWizards(wizards);
+  renderSimilarWizards(similarWizards);
 
 })();
